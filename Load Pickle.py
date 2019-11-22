b3f534d8ac5496d2ce284f6cc5823589aae1e766
Rijul Kumar <jiroxys101@gmail.com>
2019-11-22 13:51:01 -0500

    Updated messenger

---
+++ Load Pickle.py
@@ -0,0 +1,133 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+import random
+import pyperclip
+import unittest
+import csv
+import time
+import os
+from tkinter import *
+from selenium.webdriver.common.action_chains import ActionChains
+import re
+from datetime import datetime
+from tkinter.filedialog import askopenfilename
+from collections import OrderedDict
+import math
+import re
+import pickle
+
+
+log_in = {"Rijul Kumar": ["rijulkumar.webtrafik@gmail.com", "Gocam2020"],
+          "Omotayo Ogunnusi": ["tayoogunnusi@outlook.com", "Temitope5"],
+          "Rijul K": ["rijulkumar7500@gmail.com", "nov192011"],
+          "Prasad Pansare": ["prasadpansare2019@gmail.com", "Gocam2020"],
+          "Dieudonné Ndjebayi": ["rijul.kumar@construction.com", "Gocam2020"],
+          "Sushma Kumar": ["sush126.kumar@gmail.com", "Dmnd2017"],
+          }
+
+user_agent_list = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    #Firefox
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    ]
+
+master = Tk()
+
+ID = str(round(time.time()))
+
+
+d = datetime.now().strftime("%I:%M %p")
+start_time = time.time()
+print(d)
+print('')
+
+variable1 = StringVar(master)
+variable1.set("")  # default value
+
+w = OptionMenu(master, variable1, "Rijul Kumar", "Omotayo Ogunnusi", "Rijul K", "Prasad Pansare", "Dieudonné Ndjebayi", "Sushma Kumar")
+w.pack()
+
+name_var = ""
+email_var = ""
+pw_var = ""
+
+
+def ok():
+
+    if variable1.get() in log_in:
+        global name_var
+        name_var = variable1.get()
+        global email_var
+        email_var = log_in.get(variable1.get())[0]
+        global pw_var
+        pw_var = log_in.get(variable1.get())[1]
+        print("Name: ", name_var)
+        print("E-Mail: ", email_var)
+        print("Password: ", pw_var)
+        print('')
+
+    master.quit()
+    master.destroy()
+
+
+button = Button(master, text="OK", command=ok)
+button.pack()
+mainloop()
+
+chrome_options = webdriver.ChromeOptions()
+user_agent = random.choice(user_agent_list)
+
+bunji = os.getenv("USERNAME")
+# userProfile = "C:\\Users\\jirox_000\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+# userProfile = "C:\\Users\\" + bunji + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+chrome_options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors", "safebrowsing-disable-download-protection", "safebrowsing-disable-auto-update", "disable-client-side-phishing-detection"])
+
+# chrome_options.add_argument("user-data-dir={}".format(userProfile))
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-plugins-discovery");
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument(f'user-agent={user_agent}')
+chrome_driver = "C:\Webdrivers\\chromedriver.exe"
+
+is_URL = True
+
+print(user_agent)
+
+
+def set_up():
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    driver.get('http://www.google.com/')
+
+    cookies = pickle.load(open(str(email_var) + ".pkl", "rb"))
+    for cookie in cookies:
+        if 'expiry' in cookie:
+            cookie.pop('expiry')  # cookie['expires'] = cookie.pop('expiry')
+        driver.add_cookie(cookie)
+    driver.get('https://www.linkedin.com/feed/')
+    logo_x_path = "(//span[contains(@id, 'feed-tab-icon')])"
+    WebDriverWait(driver, 60).until(lambda driver: driver.find_element_by_xpath(logo_x_path))
+    print("Log in successful")
+
+
+set_up()
