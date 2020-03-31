483c7e85366800234a6a115085c9bb67768c2eeb
Rijul Kumar <jiroxys101@gmail.com>
2020-03-31 11:50:42 -0400

    Updates v2

---
+++ Withdraw Connections 1.0.0.py
@@ -0,0 +1,244 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+import random
+import pyperclip
+import unittest
+import csv
+import time
+import os
+from tkinter import *
+from selenium.webdriver.common.action_chains import ActionChains
+import re
+from datetime import datetime
+from tkinter.filedialog import askopenfilename
+from collections import OrderedDict
+import math
+import re
+import pickle
+from requests import get
+
+
+log_in = {"Rijul Kumar": ["rijulkumar.webtrafik@gmail.com", "Gocam2020"],
+          "Omotayo Ogunnusi": ["tayoogunnusi@outlook.com", "Temitope5"],
+          "Rijul K": ["rijulkumar7500@gmail.com", "nov192011"],
+          "Prasad Pansare": ["prasadpansare2019@gmail.com", "Gocam2020"],
+          "Dieudonné Ndjebayi": ["rijul.kumar@construction.com", "Gocam2020"],
+          "Sushma Kumar": ["sush126.kumar@gmail.com", "Dmnd2017"],
+          }
+
+user_agent_list = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    #Firefox
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    ]
+
+master = Tk()
+
+ID = str(round(time.time()))
+
+
+d = datetime.now().strftime("%I:%M %p")
+start_time = time.time()
+print(d)
+print('')
+
+variable1 = StringVar(master)
+variable1.set("")  # default value
+
+w = OptionMenu(master, variable1, "Rijul Kumar", "Omotayo Ogunnusi", "Rijul K", "Prasad Pansare", "Dieudonné Ndjebayi", "Sushma Kumar")
+w.pack()
+
+name_var = ""
+email_var = ""
+pw_var = ""
+
+
+def ok():
+
+    if variable1.get() in log_in:
+        global name_var
+        name_var = variable1.get()
+        global email_var
+        email_var = log_in.get(variable1.get())[0]
+        global pw_var
+        pw_var = log_in.get(variable1.get())[1]
+        print("Name: ", name_var)
+        print("E-Mail: ", email_var)
+        print("Password: ", pw_var)
+        print('')
+
+    master.quit()
+    master.destroy()
+
+
+button = Button(master, text="OK", command=ok)
+button.pack()
+mainloop()
+
+chrome_options = webdriver.ChromeOptions()
+user_agent = random.choice(user_agent_list)
+
+bunji = os.getenv("USERNAME")
+# userProfile = "C:\\Users\\jirox_000\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+# userProfile = "C:\\Users\\" + bunji + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+chrome_options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors", "safebrowsing-disable-download-protection", "safebrowsing-disable-auto-update", "disable-client-side-phishing-detection"])
+
+# chrome_options.add_argument("user-data-dir={}".format(userProfile))
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--profile-directory=Default')
+# chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-plugins-discovery");
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument(f'user-agent={user_agent}')
+chrome_driver = "C:\Webdrivers\\chromedriver.exe"
+
+is_int = True
+while is_int:
+    pause_time = int(input("Enter how many seconds you would like to pause for:"))
+    if isinstance(pause_time, int):
+        is_int = False
+    else:
+        is_int = True
+        print("Sorry.  That value is not an integer.", end=" | ")
+
+print("Pausing for " + str(pause_time) + " seconds")
+
+
+def countdown(t):
+    while t >= 0:
+        sys.stdout.write('\r' + str(t) + ' seconds left')
+        sys.stdout.flush()
+        time.sleep(1)
+        t -= 1
+    print(" Starting!")
+
+
+is_URL = True
+
+print(user_agent)
+
+try:
+    os.system('tasklist | find /i "CyberGhost.exe" && taskkill /im CyberGhost.exe /F || echo VPN not running')
+except Exception:
+    print('...', end="")
+finally:
+    time.sleep(5)
+
+try:
+    os.system('tasklist | find /i "chromedriver.exe" && taskkill /im chromedriver.exe /F || echo chromedriver not running')
+except Exception:
+    print('...', end="")
+finally:
+    time.sleep(5)
+
+
+def set_up():
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    print("bb")
+    if pause_time > 0:
+        driver.get('http://www.google.com')
+        countdown(pause_time)
+
+    ip1 = str(get('https://api.ipify.org').text)
+    print(ip1, end="")
+    os.startfile('C:\\Program Files\\CyberGhost 7\\CyberGhost.exe')
+    ip2 = ip1
+    while True:
+        try:
+            ip2 = str(get('https://api.ipify.org').text)
+        except Exception:
+            pass
+        finally:
+            if ip2 != ip1:
+                ip = str(get('https://api.ipify.org').text)
+                print(ip)
+                break
+            else:
+                print('.', end="")
+                time.sleep(1)
+
+    driver.get('https://www.linkedin.com/uas/login?trk=guest_homepage-basic_nav-header-signin')
+    driver.set_page_load_timeout(600)
+
+    username = str(email_var)
+    password = str(pw_var)
+    email_field_id = "username"
+    pass_field_id = "password"
+    login_button_xpath = "//button[contains(@type,'submit')]"
+    print("Logging in", end="")
+    driver.save_screenshot('screenie.png')
+
+    email_field_element = WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_id(email_field_id))
+    pass_field_element = WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_id(pass_field_id))
+    login_button_element = WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_xpath(
+        login_button_xpath))
+    print(".", end="")
+    email_field_element.clear()
+    email_field_element.send_keys(username)
+    pass_field_element.clear()
+    pass_field_element.send_keys(password)
+    print(".", end="")
+    login_button_element.click()
+    print(".", end="")
+    logo_x_path = "(//span[contains(@id, 'feed-tab-icon')])"
+    WebDriverWait(driver, 60).until(lambda driver: driver.find_element_by_xpath(logo_x_path))
+    print("Log in successful")
+
+    driver.get('https://www.linkedin.com/mynetwork/invitation-manager/sent/')
+    disq_time = ['week', 'month']
+    len1 = 0
+    len2 = 0
+
+    elements = []
+    past = []
+    withdraw = []
+    reit = True
+
+    while True:
+        if reit:
+            try:
+                past = WebDriverWait(driver, 60).until(lambda driver: driver.find_elements_by_xpath("//time[contains(@class, 'time-badge')]")).get_attribute('innerText')
+            except TimeoutException:
+                print("No Invitations Found")
+                break
+            finally:
+                print(len(past))
+                try:
+                    withdraw = WebDriverWait(driver, 60).until(lambda driver: driver.find_elements_by_xpath("//button[contains(@data-control-name, 'single')]")).get_attribute('innerText')
+                except TimeoutException:
+                    print("No buttons Found")
+                    break
+                else:
+                    print(len(withdraw))
+            reit = False
+            elements = list(zip(past, withdraw))
+        if (len(past) == 0) or (len(withdraw) == 0):
+            break
+        else:
+            for e in elements:
+                print(str(e))
+        break
+
+    pickle.dump(driver.get_cookies(), open(str(email_var) + ".pkl", "wb"))
+
+
+set_up()
